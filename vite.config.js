4e7c9faec19ada49d6bccddc990fd05250ada0b9
Oscar <DeveloperMAC2023@gmail.com>
2025-02-01 13:30:33 -0300

    Estructura de archivos de entrada VUE

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                //'resources/css/app.css', 'resources/js/app.js'
+
+                // BackOffice
+                'resources/sass/backoffice.scss',
+                'resources/js/backoffice.js',
+
+                // FrontEnd (Site)
+                'resources/sass/site.scss',
+                'resources/js/site.js'
+
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler',
+            '@': path.resolve(__dirname, './resources/js')
+        }
+    }
 });
